f47050045817b395637091d2f0a7ae18a65dd042
GemmandKilua2010 <specterxofc@outlook.com>
2026-09-02 22:34:22 -0300

    Update music.lua

--- music.lua
+++ music.lua
@@ -2,19 +2,32 @@ local MarketplaceService = game:GetService("MarketplaceService")
 local MUSIC_LIST = loadstring(game:HttpGet("https://raw.githubusercontent.com/GemmandKilua2010/Music/main/list.lua"))()
 
 local ValidMusic = {}
+local Pending = 0
 
 for name, id in pairs(MUSIC_LIST) do
-    local success, info = pcall(function()
-        return MarketplaceService:GetProductInfo(
-            tonumber(id),
-            Enum.InfoType.Asset
-        )
-    end)
+    Pending += 1
 
-    if success and info then
-        ValidMusic[name] = id
-        print("FUNCIONOU:", name, id)
-    end
+    task.spawn(function()
+        local success, info = pcall(function()
+            return MarketplaceService:GetProductInfo(
+                tonumber(id),
+                Enum.InfoType.Asset
+            )
+        end)
+
+        if success and info and info.AssetTypeId == 3 then
+            ValidMusic[name] = id
+            print("FUNCIONOU:", name, id)
+        else
+            print("IGNORADO:", name, id)
+        end
+
+        Pending -= 1
+    end)
 end
+
+repeat
+    task.wait()
+until Pending == 0
 
 return ValidMusic
